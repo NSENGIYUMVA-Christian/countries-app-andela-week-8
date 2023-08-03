9f9873070ac849de6780dc142b08b48febd60eb7
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-08-03 13:37:32 +0300

    done with getting all data function from api

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log("hello js");
+let defaultCountryName = "rwanda";
+
+async function getAllData(countryName) {
+  try {
+    let data = await fetch(
+      `https://restcountries.com/v3.1/name/${countryName}`,
+      {
+        mode: "cors",
+      }
+    );
+
+    let response = await data.json();
+
+    console.log(response);
+    console.log("okk");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// default run on the first render
+getAllData(defaultCountryName);
